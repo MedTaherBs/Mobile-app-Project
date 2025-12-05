ac9b6a92d013d3d4023d47f539d840d0fa068ec0
MedTaherBs <benslamamohamedtaher@isimsf.u-sfax.tn>
2025-12-05 14:06:39 +0100

    Initial commit - Projet Mobile avec Firebase Auth

---
+++ app/src/main/java/com/example/projetmobile/ui/HomeScreen.kt
@@ -0,0 +1,24 @@
+package com.example.projetmobile.ui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.projetmobile.ui.theme.ProjetMobileTheme
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ProjetMobileTheme {
+        Greeting("Android")
+    }
+}
